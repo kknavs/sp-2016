41370075832cb0c5cadc2593fadcaa4bee723875
kknavs <karmen.knavs@gmail.com>
2016-11-27 23:33:30 +0100

    Activity and settings js.

---
+++ js/settings.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    settingsChange();
+})
+
+function settingsChange() {
+    //document.getElementsByTagName("form").style.display = "none";
+    document.getElementById("formNotifications").style.display = "none";
+    document.getElementById("formEmail").style.display = "none";
+    document.getElementById("formPassword").style.display = "none";
+    var x = document.getElementById("settingsChange").value;
+    document.getElementById("form"+x).style.display = "block";
+}
